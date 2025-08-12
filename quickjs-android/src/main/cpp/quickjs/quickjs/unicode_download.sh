e28b3c72d14c40cfb09d6be44d60ed8f4b278a39
qqty <2824157238@qq.com>
2025-08-12 11:34:03 +0800

    更新 quickjs v2025-04-26

--- quickjs-android/src/main/cpp/quickjs/quickjs/unicode_download.sh
+++ quickjs-android/src/main/cpp/quickjs/quickjs/unicode_download.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 set -e
 
-url="ftp://ftp.unicode.org/Public/13.0.0/ucd"
-emoji_url="${url}/emoji/emoji-data.txt"
+version="16.0.0"
+emoji_version="16.0"
+url="ftp://ftp.unicode.org/Public"
 
 files="CaseFolding.txt DerivedNormalizationProps.txt PropList.txt \
 SpecialCasing.txt CompositionExclusions.txt ScriptExtensions.txt \
@@ -11,9 +12,12 @@ PropertyValueAliases.txt"
 
 mkdir -p unicode
 
-#for f in $files; do
-#    g="${url}/${f}"
-#    wget $g -O unicode/$f
-#done
-    
-wget $emoji_url -O unicode/emoji-data.txt
+for f in $files; do
+    g="${url}/${version}/ucd/${f}"
+    wget $g -O unicode/$f
+done
+
+wget "${url}/${version}/ucd/emoji/emoji-data.txt" -O unicode/emoji-data.txt
+
+wget "${url}/emoji/${emoji_version}/emoji-sequences.txt" -O unicode/emoji-sequences.txt
+wget "${url}/emoji/${emoji_version}/emoji-zwj-sequences.txt" -O unicode/emoji-zwj-sequences.txt
